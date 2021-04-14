b5bbd5d733aca5d1a313e5e3fa16277f2f7d9401
Dennis Neufeld <git@dneufeld.net>
2021-04-14 09:30:19 +0200

    Added fr24feed DUMP1090_HOST env variable

--- fr24feed/entrypoint.sh
+++ fr24feed/entrypoint.sh
@@ -2,9 +2,11 @@
 
 # Environment
 SHARING_KEY="${SHARING_KEY:-}"
+DUMP1090_HOST="${DUMP1090_HOST:-dump1090:30002}"
 
 # Set variables
 sed -i "s/{{SHARING_KEY}}/$SHARING_KEY/g" /etc/fr24feed.ini
+sed -i "s/{{DUMP1090_HOST}}/$DUMP1090_HOST/g" /etc/fr24feed.ini
 
 # Exec CMD
 exec "$@"
