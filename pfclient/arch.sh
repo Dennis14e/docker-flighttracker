d67be9b2d84a11961efd5adf23e6cfccafd9ae9c
Dennis Neufeld <git@dneufeld.net>
2021-09-25 13:10:35 +0200

    Retry using alpine instead of debian

--- pfclient/arch.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if [ `uname -m` = "aarch64" ]
-then
-    dpkg --add-architecture armhf
-    apt-get update
-    apt-get install --no-install-recommends -y libc6:armhf
-    rm -rf /var/lib/apt/lists/*
-fi
